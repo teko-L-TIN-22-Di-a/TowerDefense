e0b9298a6936fe4b547234a2b5cf950578361cda
Yillie <tobia.schaerli@hotmail.com>
2024-09-03 08:54:14 +0200

    Remove Java Docs

--- Docs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"enemies"},{"l":"inputs"},{"l":"main"},{"l":"maps"},{"l":"scenes"},{"l":"towers"}];updateSearchResults();
